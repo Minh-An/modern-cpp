1c92b8aa4b84c31853ce5bc1432ed35ddfef2fbc
An Doan <andoan@stanford.edu>
2023-10-08 14:22:44 -0700

    Code c++20 implementation of generators.

---
+++ generators/gen3.cpp
@@ -0,0 +1,93 @@
+/*
+ * This implementation of generators is the modern C++20 implementation
+ * using coroutines.
+ */
+
+#include <coroutine>
+#include <iostream> 
+
+template <typename T> 
+struct generator {
+  struct promise_type;
+
+  using handle_type = std::coroutine_handle<promise_type>;
+  handle_type handle;
+
+  struct promise_type {
+    T current_value;
+
+    auto get_return_object() {
+      return generator(*this);
+    }
+
+    std::suspend_always initial_suspend() {
+      //return std::experimental::suspend_always();
+      return {};
+    }
+
+    std::suspend_always  final_suspend() noexcept {
+      //return std::experimental::suspend_always();
+      return {};
+    }
+
+    std::suspend_always  yield_value(const T& value) {
+      current_value = value;
+      //return std::experimental::suspend_always();
+      return {};
+    }
+
+    void return_void() {
+    }
+
+    void unhandled_exception() {
+      std::terminate();
+    }
+
+  };
+
+  generator(promise_type& promise) : handle(handle_type::from_promise(promise)) {
+
+  }
+
+  generator(const generator&) = delete;
+
+  generator& operator=(const generator&) = delete;
+
+  generator(generator&& other) : handle(other.handle) {
+    other.handle = nullptr;
+  }
+
+  generator& operator=(generator&& other) {
+    if (this != &other) {
+      handle = other.handle;
+      other.handle = nullptr;
+    }
+  }
+
+  ~generator() {
+    if (handle) {
+      handle.destroy();
+    }
+  }
+
+  T operator()() {
+    assert(handle != nullptr);
+    handle.resume();
+    return handle.promise().current_value;
+  }
+};
+
+generator<int> f() {
+  int i = 0;
+  while (true) {
+    co_yield i++;
+  }
+}
+
+
+int main() {
+  auto g = f();
+  std::cout << g() << std::endl;
+  std::cout << g() << std::endl;
+  std::cout << g() << std::endl;
+}
